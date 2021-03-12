fdc980b67c1d104e55ed322cae09f805f5f24602
Nilanjan Siromani <Nilanjan.Siromani@walmartlabs.com>
2021-03-12 20:27:22 +0530

    arrange by hostname

--- background.js
+++ background.js
@@ -1,14 +1,38 @@
-chrome.commands.onCommand.addListener(function(command) {
+chrome.commands.onCommand.addListener(function (command) {
   if (command == "toggle-pin") {
     // Get the currently selected tab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       var currentTab = tabs[0];
-      chrome.tabs.group({
-        tabIds: currentTab.id
-      }, (tabGroupId) => {
-        console.log(tabGroupId);
-        chrome.tabGroups.update(tabGroupId, {"title": currentTab.title})
-      })
+      chrome.tabs.group(
+        {
+          tabIds: currentTab.id,
+        },
+        (tabGroupId) => {
+          chrome.tabGroups.update(tabGroupId, { title: currentTab.title });
+        }
+      );
+    });
+  }
+  if (command == "arrange-all") {
+    chrome.tabs.query({}, (list) => {
+      const uniqueTabHosts = {};
+      list.forEach((tab) => {
+        const url = new URL(tab.url);
+        if (!uniqueTabHosts[url.hostname]) {
+          uniqueTabHosts[url.hostname] = [];
+        }
+        uniqueTabHosts[url.hostname].push(tab.id);
+      });
+      for (const tabGroup in uniqueTabHosts) {
+        chrome.tabs.group(
+          {
+            tabIds: uniqueTabHosts[tabGroup],
+          },
+          (tabGroupId) => {
+            chrome.tabGroups.update(tabGroupId, { title: tabGroup });
+          }
+        );
+      }
     });
   }
 });
