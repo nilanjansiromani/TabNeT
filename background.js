7387bac28f9fee10b23f3c6b5c71a95ec9e7d50d
nilajansiromani <siromani.n@gmail.com>
2021-05-22 19:41:27 +0530

    remove redundant commands

--- background.js
+++ background.js
@@ -1,18 +1,5 @@
 chrome.commands.onCommand.addListener(function (command) {
-  if (command == "addtogroup") {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      var currentTab = tabs[0];
-      chrome.tabs.group(
-        {
-          tabIds: currentTab.id,
-        },
-        (tabGroupId) => {
-          chrome.tabGroups.update(tabGroupId, { title: currentTab.title });
-        }
-      );
-    });
-  }
-  if (command == "arrange-all") {
+  if (command == "autoArrangeTabs") {
     chrome.tabs.query({}, (list) => {
       const uniqueTabHosts = {};
       list.forEach((tab) => {
@@ -72,19 +59,6 @@ chrome.commands.onCommand.addListener(function (command) {
           }
         });
       }
-    });
-  }
-  if (command == "pintab") {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      var current = tabs[0];
-      chrome.tabs.update(current.id, { pinned: !current.pinned });
-    });
-  }
-  if (command == "discard") {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      var current = tabs[0];
-      chrome.tabs.discard(current.id, () => console.log("Tab discarded"));
-      alert("Page has been hibernated");
     });
   }
 });
