de5019a38c29781937f64ee035a151d75f9f6dde
nilajansiromani <siromani.n@gmail.com>
2021-05-22 20:07:38 +0530

    need to fix the data Structure

--- background.js
+++ background.js
@@ -1,39 +1,48 @@
 const getDomainName = (url) => {
-  url = new URL(tab.url);
-  let sitename = url.hostname.split(".").slice(-2)[0];
-  if (sitename === "walmart") {
-    sitename = url.hostname.split(".").slice(-3)[0];
+  url = new URL(url);
+  let domainName = url.hostname.split(".").slice(-2)[0];
+  if (domainName === "walmart") {
+    domainName = url.hostname.split(".").slice(-3)[0];
   }
-  return sitename;
+  return domainName;
+};
+
+const uniqueTabHosts = {};
+
+const autoArrangeTabsByDomain = (list) => {
+  list.forEach((tab) => {
+    const domainName = getDomainName(tab.url);
+    if (!uniqueTabHosts[domainName]) {
+      uniqueTabHosts[domainName] = [];
+    }
+    uniqueTabHosts[domainName].push(tab.id);
+  });
+  for (const tabGroup in uniqueTabHosts) {
+    chrome.tabs.group(
+      {
+        tabIds: uniqueTabHosts[tabGroup],
+      },
+      (tabGroupId) => {
+        chrome.tabGroups.update(tabGroupId, {
+          collapsed: true,
+          title: tabGroup,
+        });
+      }
+    );
+  }
 };
 
 chrome.commands.onCommand.addListener(function (command) {
   if (command == "autoArrangeTabs") {
     chrome.tabs.query({}, (list) => {
-      const uniqueTabHosts = {};
-      list.forEach((tab) => {
-        if (!uniqueTabHosts[sitename]) {
-          uniqueTabHosts[sitename] = [];
-        }
-        uniqueTabHosts[sitename].push(tab.id);
-      });
-      for (const tabGroup in uniqueTabHosts) {
-        chrome.tabs.group(
-          {
-            tabIds: uniqueTabHosts[tabGroup],
-          },
-          (tabGroupId) => {
-            chrome.tabGroups.update(tabGroupId, {
-              collapsed: true,
-              title: tabGroup,
-            });
-          }
-        );
-      }
+      autoArrangeTabsByDomain(list);
     });
     chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+      console.log(tabId);
+      console.log(changeInfo);
+      console.log;
       if (tab.status === "complete") {
-        console.log("The tab has been loaded");
+        console.table(uniqueTabHosts);
       }
     });
   }
