59bfcc4dea40b918018736fa4e6b491a068f4b41
nilajansiromani <siromani.n@gmail.com>
2021-07-30 15:05:59 +0530

    refactor

--- background.js
+++ background.js
@@ -6,4 +6,26 @@ chrome.commands.onCommand.addListener(function (command) {
       chrome.tabs.update(current.id, {'pinned': !current.pinned});
     });
   }
+  if (command == "discard") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      var current = tabs[0];
+      chrome.tabs.discard(current.id, () => console.log("Tab discarded"));
+      alert("Page has been hibernated");
+    });
+  }
+  if (command == "addtogroup") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.group(
+        {
+          tabIds: tabs[0].id,
+        },
+        (tabGroupId) => {
+          chrome.tabGroups.update(tabGroupId, {
+            collapsed: false,
+            title: tabs[0].title.split(" ")[0],
+          });
+        }
+      );
+    });
+  }
 });
